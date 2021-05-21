9faccdaa7950341f5a31490bb570e36acb945a49
Satyendra Singh <SSingh@thinklink.io>
2021-05-21 17:33:49 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+setuptools.setup(
+    name="Thinklink.io",
+    version="1.0",
+    author="Thinklink",
+    author_email="info@thinklink.io",
+    description="This is a test package.",
+    long_description="big description",
+    long_description_content_type="text/markdown",
+    url="package_bitbucket_page",
+    packages=['mylib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: ThinkLink",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
